146e4c76db095a4c5b896447e722dd45bb8d1fd7
Christian-GL <christiangl202@hotmail.com>
2025-03-15 21:15:01 +0100

    Primer ejercicio de iniciación en PHP

---
+++ index1.php
@@ -0,0 +1,35 @@
+
+<?php
+echo "¡Hola, PHP en VS Code!";
+
+$room1 = [
+    "id" => 1,
+    "name" => "Suite",
+    "number" => 101,
+    "price" => 150.00,
+    "discount" => 10
+];
+
+$room2 = [
+    "id" => 2,
+    "name" => "Suite Deluxe",
+    "number" => 102,
+    "price" => 175.00,
+    "discount" => 20
+];
+
+$room3 = [
+    "id" => 3,
+    "name" => "Suite Extra Deluxe",
+    "number" => 103,
+    "price" => 200.00,
+    "discount" => 30
+];
+
+$habitaciones = [$room1, $room2, $room3];
+
+echo "<pre>";
+print_r($habitaciones);
+echo "</pre>";
+
+?>
